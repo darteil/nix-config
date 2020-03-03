a81d6e73988ee23e635a555acc11c0f32b25bae6
Romanov Yuri <darteil.base@gmail.com>
2020-03-03 16:36:27 +0700

    update rofi

---
+++ .config/rofi/scripts/powermenu.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+rofi_command="rofi -theme powermenu.rasi -lines 2 -dmenu -p power"
+option_reboot=" reboot"
+option_shutdown=" shutdown"
+
+options="$option_reboot\n$option_shutdown"
+
+chosen="$(echo -e "$options" | $rofi_command)"
+case $chosen in
+    $option_reboot)
+        systemctl reboot;;
+    $option_shutdown)
+        systemctl poweroff;;
+esac
