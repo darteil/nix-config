9abe01985126527ecc2be3e8f81a43d071c5122c
Romanov Yuri <darteil.base@gmail.com>
2022-03-22 12:54:30 +0700

    update i3 config

--- i3wm/.config/i3/lock.sh
+++ i3wm/.config/i3/lock.sh
@@ -1,1 +1,45 @@
-i3lock -t -u -e -i  ~/.config/i3/lock.png
+#Positions for 1920x1080
+i3lock -n -e \
+  --color="#282828" \
+  --inside-color="#282828" \
+  --ring-color="#ebdbb2" \
+  --line-uses-inside \
+  --keyhl-color="#458588" \
+  --bshl-color="#458588" \
+  --insidever-color="#d65d0e" \
+  --insidewrong-color="#cc241d" \
+  --ringver-color="#ebdbb2" \
+  --ringwrong-color="#ebdbb2" \
+  --date-color="#ebdbb2" \
+  --time-color="#ebdbb2" \
+  --layout-color="#ebdbb2" \
+  \
+  --ind-pos="x+65:y+980" \
+  --time-pos="x+1800:y+980" \
+  --layout-pos="x+130:y+985" \
+  \
+  --ring-width 8 \
+  --time-align 2 \
+  --date-align 2 \
+  --no-modkey-text \
+  --radius 20 \
+  \
+  --time-str="%H:%M" \
+  --date-str="%A %e %B %Y" \
+  --noinput-text="" \
+  --verif-text="" \
+  --lock-text="" \
+  --wrong-text="" \
+  \
+  --time-font="Ubuntu" \
+  --date-font="Ubuntu" \
+  --layout-font="Ubuntu" \
+  \
+  --time-size 36 \
+  --date-size 18 \
+  --layout-size 18 \
+  \
+  --indicator \
+  --keylayout 1 \
+  --clock \
+  --screen 1
