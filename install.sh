2c8d3cebfaeb6350bd9ec6f1a55e7b5553061487
Romanov Yuri <darteil.base@gmail.com>
2022-04-15 14:29:50 +0700

    migrate to bspwm

--- install.sh
+++ install.sh
@@ -6,7 +6,6 @@ stow -v -R -t ~/ conky
 stow -v -R -t ~/ cheatsheet
 stow -v -R -t ~/ bat
 stow -v -R -t ~/ dunst
-stow -v -R -t ~/ i3wm
 stow -v -R -t ~/ kitty
 stow -v -R -t ~/ nvim
 stow -v -R -t ~/ polybar
@@ -14,3 +13,5 @@ stow -v -R -t ~/ rofi
 stow -v -R -t ~/.oh-my-zsh/ oh-my-zsh
 stow -v -R -t ~/.icons cursor
 stow -v -R -t ~/.config/vifm vifm
+stow -v -R -t ~/ bspwm
+stow -v -R -t ~/ sxhkd
