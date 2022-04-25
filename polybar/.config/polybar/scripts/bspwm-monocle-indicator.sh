d33b7409e172d6dafe3d498291dd3c1d650a8a0a
Romanov Yuri <darteil.base@gmail.com>
2022-04-25 16:04:30 +0700

    polybar: fix monocle indicator script

--- polybar/.config/polybar/scripts/bspwm-monocle-indicator.sh
+++ polybar/.config/polybar/scripts/bspwm-monocle-indicator.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-monitor="HDMI-0"
-
 bspc subscribe desktop_focus desktop_layout node_remove node_add node_transfer | while read -r _; do
 
-    monocle=$(bspc query -D -m "$monitor" focused.monocle)
+    monocle=$(bspc query -D -d focused.monocle)
 
     if [[ $monocle ]]; then
-      desktop=$(bspc query -D -m "$monitor" -d --names)
+      desktop=$(bspc query -D -d focused --names)
       number_of_windows=$(bspc query -N -d "$desktop" -n .window | wc -l)
       echo "[M]=$number_of_windows"
     else
