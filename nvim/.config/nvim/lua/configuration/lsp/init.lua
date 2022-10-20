d051095ca3ea2fb53644065574b0c4473b881a43
Romanov Yuri <darteil.base@gmail.com>
2022-10-20 12:11:41 +0700

    fix cmp broken change

--- nvim/.config/nvim/lua/configuration/lsp/init.lua
+++ nvim/.config/nvim/lua/configuration/lsp/init.lua
@@ -34,7 +34,7 @@ local on_attach = function(client)
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 local servers = { "tsserver", "html", "cssls", "bashls", "gopls" }
