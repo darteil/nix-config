882ade1a4bdfa0d842998e0d7fa17cd66c11b41a
Romanov Yuri <darteil.base@gmail.com>
2023-01-27 10:57:52 +0700

    remove noice plugin

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -53,11 +53,4 @@ return packer.startup(function()
   use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
   use({ "jose-elias-alvarez/buftabline.nvim" })
   use({ "numToStr/Navigator.nvim", commit = "6bfcf7d" })
-  -- use({
-  --   "folke/noice.nvim",
-  --   requires = {
-  --     "MunifTanjim/nui.nvim",
-  --     "rcarriga/nvim-notify",
-  --   },
-  -- })
 end)
