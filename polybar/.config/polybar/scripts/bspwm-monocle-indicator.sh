e4110ccd14abae9934f072dcc148675935808de7
Romanov Yuri <darteil.base@gmail.com>
2023-03-06 07:11:51 +0700

    update polybar config

--- polybar/.config/polybar/scripts/bspwm-monocle-indicator.sh
+++ polybar/.config/polybar/scripts/bspwm-monocle-indicator.sh
@@ -7,7 +7,7 @@ bspc subscribe desktop_focus desktop_layout node_remove node_add node_transfer |
     if [[ $monocle ]]; then
       desktop=$(bspc query -D -d focused --names)
       number_of_windows=$(bspc query -N -d "$desktop" -n .window | wc -l)
-      echo "  Monocle = $number_of_windows"
+      echo "  Monocle = $number_of_windows  "
     else
       echo ""
     fi
