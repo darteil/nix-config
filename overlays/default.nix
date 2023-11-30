300fc864792db06e4681654fd0d4cee1bd15cc64
Romanov Yuri <darteil.base@gmail.com>
2023-11-30 23:45:10 +0700

    add bspwm overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -8,4 +8,14 @@
   polybar = final: prev: {
     polybar = prev.polybar.override { pulseSupport = true; };
   };
+  bspwm-latest = final: prev: {
+    bspwm = prev.bspwm.overrideAttrs (old: {
+      src = prev.fetchFromGitHub {
+        owner = "baskerville";
+        repo = "bspwm";
+        rev = "af3bd8b4351f4478fe0fe3cfd6c09e44cb108b4b";
+        sha256 = "O4Qwdjb3p6jw8Qtcd4zGZ57cB3oCCbPZcjUQtWbyC7Y=";
+      };
+    });
+  };
 }
