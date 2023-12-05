7965f293d6caeaa6814bd7b2d11c8cc41118effa
Romanov Yuri <darteil.base@gmail.com>
2023-12-05 14:14:44 +0700

    remove null-ls, add conform and nvim-lint plugins

---
+++ home/dotfiles/nvim/lua/plugins/conform.lua
@@ -0,0 +1,25 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    require("conform").setup({
+      formatters_by_ft = {
+        javascript = { "prettierd" },
+        typescript = { "prettierd" },
+        javascriptreact = { "prettierd" },
+        typescriptreact = { "prettierd" },
+        css = { "prettierd" },
+        html = { "prettierd" },
+        json = { "prettierd" },
+        markdown = { "prettierd" },
+        lua = { "stylua" },
+        nix = { "nixpkgs_fmt" },
+      },
+      format_on_save = {
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 1000,
+      },
+    })
+  end,
+}
