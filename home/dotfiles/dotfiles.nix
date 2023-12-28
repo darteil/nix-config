877d9606389353759960385abe61ee9d610b7a61
Romanov Yuri <darteil.base@gmail.com>
2023-12-29 05:56:47 +0700

    nixos: refactoring

--- home/dotfiles/dotfiles.nix
+++ home/dotfiles/dotfiles.nix
@@ -1,26 +1,26 @@
 { config, ... }:
 
 let
-  userDirectory = "/home/darteil";
+  nixConfigPath = "nix-config";
   inherit (config.lib.file) mkOutOfStoreSymlink;
 in
 {
   home = {
     file = {
-      ".config/nvim".source = mkOutOfStoreSymlink "${userDirectory}/nix-config/home/dotfiles/nvim";
-      ".config/bspwm".source = mkOutOfStoreSymlink "${userDirectory}/nix-config/home/dotfiles/bspwm";
-      ".config/polybar".source = mkOutOfStoreSymlink "${userDirectory}/nix-config/home/dotfiles/polybar";
-      ".config/rofi".source = mkOutOfStoreSymlink "${userDirectory}/nix-config/home/dotfiles/rofi";
-      ".config/bat".source = mkOutOfStoreSymlink "${userDirectory}/nix-config/home/dotfiles/bat";
-      ".config/dunst".source = mkOutOfStoreSymlink "${userDirectory}/nix-config/home/dotfiles/dunst";
-      ".config/kitty".source = mkOutOfStoreSymlink "${userDirectory}/nix-config/home/dotfiles/kitty";
-      ".config/sxhkd".source = mkOutOfStoreSymlink "${userDirectory}/nix-config/home/dotfiles/sxhkd";
-      ".config/zellij".source = mkOutOfStoreSymlink "${userDirectory}/nix-config/home/dotfiles/zellij";
-      ".vifm/vifmrc".source = mkOutOfStoreSymlink "${userDirectory}/nix-config/home/dotfiles/vifm/vifmrc";
-      ".vifm/colors".source = mkOutOfStoreSymlink "${userDirectory}/nix-config/home/dotfiles/vifm/colors";
-      ".xinitrc".source = mkOutOfStoreSymlink "${userDirectory}/nix-config/home/dotfiles/xinitrc";
-      "desktop-wallpaper-1.png".source = mkOutOfStoreSymlink "${userDirectory}/nix-config/assets/desktop-wallpaper-1.png";
-      "desktop-wallpaper-2.png".source = mkOutOfStoreSymlink "${userDirectory}/nix-config/assets/desktop-wallpaper-2.png";
+      ".config/nvim".source = mkOutOfStoreSymlink "${config.home.homeDirectory}/${nixConfigPath}/home/dotfiles/nvim";
+      ".config/bspwm".source = mkOutOfStoreSymlink "${config.home.homeDirectory}/${nixConfigPath}/home/dotfiles/bspwm";
+      ".config/polybar".source = mkOutOfStoreSymlink "${config.home.homeDirectory}/${nixConfigPath}/home/dotfiles/polybar";
+      ".config/rofi".source = mkOutOfStoreSymlink "${config.home.homeDirectory}/${nixConfigPath}/home/dotfiles/rofi";
+      ".config/bat".source = mkOutOfStoreSymlink "${config.home.homeDirectory}/${nixConfigPath}/home/dotfiles/bat";
+      ".config/dunst".source = mkOutOfStoreSymlink "${config.home.homeDirectory}/${nixConfigPath}/home/dotfiles/dunst";
+      ".config/kitty".source = mkOutOfStoreSymlink "${config.home.homeDirectory}/${nixConfigPath}/home/dotfiles/kitty";
+      ".config/sxhkd".source = mkOutOfStoreSymlink "${config.home.homeDirectory}/${nixConfigPath}/home/dotfiles/sxhkd";
+      ".config/zellij".source = mkOutOfStoreSymlink "${config.home.homeDirectory}/${nixConfigPath}/home/dotfiles/zellij";
+      ".vifm/vifmrc".source = mkOutOfStoreSymlink "${config.home.homeDirectory}/${nixConfigPath}/home/dotfiles/vifm/vifmrc";
+      ".vifm/colors".source = mkOutOfStoreSymlink "${config.home.homeDirectory}/${nixConfigPath}/home/dotfiles/vifm/colors";
+      ".xinitrc".source = mkOutOfStoreSymlink "${config.home.homeDirectory}/${nixConfigPath}/home/dotfiles/xinitrc";
+      "desktop-wallpaper-1.png".source = mkOutOfStoreSymlink "${config.home.homeDirectory}/${nixConfigPath}/assets/desktop-wallpaper-1.png";
+      "desktop-wallpaper-2.png".source = mkOutOfStoreSymlink "${config.home.homeDirectory}/${nixConfigPath}/assets/desktop-wallpaper-2.png";
     };
   };
 }
