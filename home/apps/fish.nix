5d2752356073d8132c4670f7c6c3969e0bb9afd4
Romanov Yuri <darteil.base@gmail.com>
2024-03-05 16:56:56 +0700

    fish: update colors

--- home/apps/fish.nix
+++ home/apps/fish.nix
@@ -26,32 +26,32 @@
       set fish_cursor_visual block
       set fish_vi_force_cursor 1
 
-      # Gruvbox Color Palette
-      set -l foreground ebdbb2
-      set -l selection 504945
-      set -l comment 928374
-      set -l red cc241d
-      set -l orange d65d0e
-      set -l yellow d79921
-      set -l green 98971a
-      set -l purple b16286
-      set -l cyan 689d5a
-      set -l blue 458588
+      set -l foreground DCD7BA normal
+      set -l selection 2D4F67 brcyan
+      set -l comment 727169 brblack
+      set -l red C34043 red
+      set -l orange FF9E64 brred
+      set -l yellow C0A36E yellow
+      set -l green 76946A green
+      set -l purple 957FB8 magenta
+      set -l cyan 7AA89F cyan
+      set -l pink D27E99 brmagenta
 
       # Syntax Highlighting Colors
       set -g fish_color_normal $foreground
       set -g fish_color_command $cyan
-      set -g fish_color_keyword $blue
+      set -g fish_color_keyword $pink
       set -g fish_color_quote $yellow
+
       set -g fish_color_redirection $foreground
       set -g fish_color_end $orange
       set -g fish_color_error $red
-      set -g fish_color_param $foreground
+      set -g fish_color_param $purple
       set -g fish_color_comment $comment
       set -g fish_color_selection --background=$selection
       set -g fish_color_search_match --background=$selection
       set -g fish_color_operator $green
-      set -g fish_color_escape $blue
+      set -g fish_color_escape $pink
       set -g fish_color_autosuggestion $comment
 
       # Completion Pager Colors
@@ -59,14 +59,13 @@
       set -g fish_pager_color_prefix $cyan
       set -g fish_pager_color_completion $foreground
       set -g fish_pager_color_description $comment
-      set -g fish_pager_color_selected_background --background=$blue
 
 
       function fish_prompt
-        set -l green (set_color 98971a)
-        set -l normal (set_color ebdbb2)
-        set -l yellow (set_color d79921)
-        set -l purple (set_color b16286)
+        set -l green (set_color 76946A)
+        set -l normal (set_color DCD7BA)
+        set -l yellow (set_color C0A36E)
+        set -l purple (set_color 957FB8)
 
         set -l nix_shell_info (
           if test -n "$IN_NIX_SHELL"
