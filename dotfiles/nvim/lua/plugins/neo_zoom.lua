f985409e2fe2c95d7ab6f8c0ec1c4791fabe0f3a
Romanov Yuri <darteil.base@gmail.com>
2024-06-19 14:58:32 +0700

    nvim: update neo-zoom config

--- dotfiles/nvim/lua/plugins/neo_zoom.lua
+++ dotfiles/nvim/lua/plugins/neo_zoom.lua
@@ -1,7 +1,16 @@
 return {
   "nyngwang/NeoZoom.lua",
   config = function()
-    require("neo-zoom").setup({})
+    require("neo-zoom").setup({
+      exclude_buftypes = { "nofile" },
+      winopts = {
+        offset = {
+          top = "center",
+          left = "center",
+        },
+        border = "single",
+      },
+    })
     vim.keymap.set("n", "<Leader>z", function()
       vim.cmd("NeoZoomToggle")
     end, { silent = true, nowait = true })
