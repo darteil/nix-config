98710b7db36a97fbb0da46c14d141d64d8ae8214
Romanov Yuri <darteil.base@gmail.com>
2024-06-25 16:16:29 +0700

    nvim: add tfm plugin

---
+++ dotfiles/nvim/lua/plugins/tfm.lua
@@ -0,0 +1,15 @@
+return {
+  "rolv-apneseth/tfm.nvim",
+  opts = {
+    file_manager = "vifm",
+    replace_netrw = true,
+    enable_cmds = true,
+    ui = {
+      border = "single",
+      height = 0.9,
+      width = 0.9,
+      x = 0.5,
+      y = 0.5,
+    },
+  },
+}
