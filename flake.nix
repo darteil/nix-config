c3f2e7290536d5aa04bf6edb777eee824ecd2272
Romanov Yuri <darteil.base@gmail.com>
2024-12-17 12:02:13 +0700

    nixos: migrate to nixos-24.11

--- flake.nix
+++ flake.nix
@@ -2,12 +2,12 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     vscode-server.url = "github:nix-community/nixos-vscode-server";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
