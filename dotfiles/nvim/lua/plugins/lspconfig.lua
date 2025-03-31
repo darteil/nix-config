604a046f90ab60cd28beb44390b81783d8090066
Romanov Yuri <darteil.base@gmail.com>
2025-03-31 20:26:32 +0700

    nvim: lsp virtual lines config

--- dotfiles/nvim/lua/plugins/lspconfig.lua
+++ dotfiles/nvim/lua/plugins/lspconfig.lua
@@ -8,8 +8,6 @@ return {
     local cmp_nvim_lsp = require("cmp_nvim_lsp")
     local schemastore = require("schemastore")
 
-    require("lspconfig.ui.windows").default_options.border = "single"
-
     -- Mappings.
     local function map(mode, key, result, opts)
       opts = vim.tbl_extend("keep", opts or {}, {
@@ -17,15 +15,21 @@ return {
         silent = true,
         expr = false,
       })
-      vim.api.nvim_set_keymap(mode, key, result, opts)
+      vim.keymap.set(mode, key, result, opts)
     end
 
     local on_attach = function(client)
       local opts = { noremap = true, silent = true }
 
-      map("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
+      map("n", "K", function()
+        vim.lsp.buf.hover({ border = "single", max_height = 25 })
+      end, opts)
       map("n", "<Leader>r", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-      map("n", "<Leader>d", "<cmd>lua vim.diagnostic.open_float(0, {scope='line'})<CR>", opts)
+      map("n", "<Leader>d", function()
+        vim.diagnostic.config({
+          virtual_lines = not vim.diagnostic.config().virtual_lines,
+        })
+      end, opts)
 
       -- Disable Autoformat
       client.server_capabilities.documentFormattingProvider = false
@@ -88,18 +92,11 @@ return {
       },
     })
 
-    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-      border = "single",
-    })
-
-    vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-      border = "single",
-    })
-
     vim.diagnostic.config({
       virtual_text = false,
+      virtual_lines = false,
       signs = true,
-      underline = true,
+      underline = false,
       severity_sort = true,
       float = {
         border = "single",
