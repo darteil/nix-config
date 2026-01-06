dc962bb87f17f630b575d44d725cae9619859f3d
Romanov Yuri <darteil.base@gmail.com>
2026-01-07 05:08:40 +0700

    nixos: update config for zapret

---
+++ hosts/default/zapret-whitelist.nix
@@ -0,0 +1,11 @@
+{
+  whitelist = [
+    "youtube.com"
+    "googlevideo.com"
+    "ytimg.com"
+    "youtu.be"
+    "discord.com"
+    "discord-attachmets-uploads-prd.storage.googleapis.com"
+    "googleapis.com"
+  ];
+}
--- hosts/default/zapret.nix
+++ hosts/default/zapret.nix
@@ -1,16 +1,11 @@
 { ... }:
+let
+  list = import ./zapret-whitelist.nix;
+in
 {
   services.zapret = {
     enable = true;
-    whitelist = [
-      "youtube.com"
-      "googlevideo.com"
-      "ytimg.com"
-      "youtu.be"
-      "discord.com"
-      "discord-attachmets-uploads-prd.storage.googleapis.com"
-      "googleapis.com"
-    ];
+    whitelist = list.whitelist;
     params = [
       "--dpi-desync=fake,disorder2"
       "--dpi-desync-ttl=1"
