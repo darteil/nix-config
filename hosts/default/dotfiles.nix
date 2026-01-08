213a9fc7448af420a3a614473b9616f410835506
Romanov Yuri <darteil.base@gmail.com>
2026-01-08 21:26:35 +0700

    nixos: remove unused dotfiles

--- hosts/default/dotfiles.nix
+++ hosts/default/dotfiles.nix
@@ -12,7 +12,6 @@ in
       ".config/nvim".source = mkOutOfStoreSymlink "${vars.nixConfigPath}/dotfiles/nvim";
       ".config/bat".source = mkOutOfStoreSymlink "${vars.nixConfigPath}/dotfiles/bat";
       ".config/wezterm".source = mkOutOfStoreSymlink "${vars.nixConfigPath}/dotfiles/wezterm";
-      ".config/alacritty".source = mkOutOfStoreSymlink "${vars.nixConfigPath}/dotfiles/alacritty";
       ".vifm/vifmrc".source = mkOutOfStoreSymlink "${vars.nixConfigPath}/dotfiles/vifm/vifmrc";
     };
   };
