638cb0e97bf8d70d10c7cb98772ed4b0fc599dc1
Romanov Yuri <darteil.base@gmail.com>
2026-02-08 13:26:29 +0700

    nvim: update keymappings for bufexplorer

--- dotfiles/nvim/lua/plugins/bufexplorer.lua
+++ dotfiles/nvim/lua/plugins/bufexplorer.lua
@@ -8,5 +8,25 @@ return {
     g.bufExplorerDefaultHelp = 1
 
     vim.keymap.set("n", "<Tab>", ":BufExplorer<CR>", { noremap = true, silent = true })
+
+    local grp = vim.api.nvim_create_augroup("UserPrefix_BufExplorerGroup", { clear = true })
+    vim.api.nvim_create_autocmd("User", {
+      group = grp,
+      pattern = "BufExplorer_Started",
+      callback = function()
+        vim.keymap.set(
+          "n",
+          "s",
+          "<Plug>(BufExplorer_OpenBufferSplitRight)",
+          { noremap = true, silent = true, buffer = true }
+        )
+        vim.keymap.set(
+          "n",
+          "S",
+          "<Plug>(BufExplorer_OpenBufferSplitBelow)",
+          { noremap = true, silent = true, buffer = true }
+        )
+      end,
+    })
   end,
 }
