fbb7f97a3330ae2f24df8babed3b1f53e08e046b
Romanov Yuri <darteil.base@gmail.com>
2026-02-25 17:52:57 +0700

    nvim: add fidget plugin

---
+++ dotfiles/nvim/lua/plugins/fidget.lua
@@ -0,0 +1,13 @@
+return {
+  "j-hui/fidget.nvim",
+  opts = {
+    progress = {
+      display = {
+        done_ttl = 3,
+        done_icon = "OK",
+        -- Highlight group for in-progress LSP tasks
+        progress_style = "Visual",
+      },
+    },
+  },
+}
