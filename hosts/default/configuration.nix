ec7197415efacd02475cf80bb6d331981119de01
Romanov Yuri <darteil.base@gmail.com>
2026-06-18 15:45:37 +0700

    nixos: replace systemd-boot with grub

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -62,8 +62,21 @@ in
     pulse.enable = true;
   };
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader = {
+    grub = {
+      enable = true;
+      device = "nodev";
+      efiSupport = true;
+      useOSProber = true;
+      gfxmodeEfi = "1920x1080";
+      splashImage = null;
+      default = 2;
+      extraConfig = ''
+        set gfxpayload=keep
+      '';
+    };
+    efi.canTouchEfiVariables = true;
+  };
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
